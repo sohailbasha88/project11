d36031b4cdf1fa46832556a88fae70e7ac33afc2
sohailbasha88 <sohailbasha88@gmail.com>
2023-12-09 22:15:58 +0530

    first commit

---
+++ app1/urls.py
@@ -0,0 +1,9 @@
+from app1.views import *
+from django.urls import path
+
+app_name='mohan'
+
+urlpartten=[
+    path('mohan/',mohan,name='mohan.html')
+]
+
---
+++ app1/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def mohan(request):
+    return render(request,'mohan.html')
